e28333a3d85fd198414a43b6a51476c06fbb4463
Stephen Flores <sf314@icloud.com>
2025-01-03 23:55:50 -0600

    feat: Add script to shutdown all pumps

---
+++ off.py
@@ -0,0 +1,20 @@
+from gpiozero import LED
+from time import sleep
+
+# GPIOZERO mapping:
+pumpPin1 = 17 # physical pin 11
+pumpPin2 = 27 # physical pin 13
+pumpPin3 = 22 # physical pin 15
+
+# Pretend they're LEDs
+print("Initializing pumps")
+pump1 = LED(pumpPin1)
+pump2 = LED(pumpPin2)
+pump3 = LED(pumpPin3)
+
+# Init
+print("Turning all pumps off")
+pump1.on()
+pump2.on()
+pump3.off()
+sleep(1)
